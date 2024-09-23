d503558194a166cbc815c33a8ab975ce6866b8e0
Erizki0712 <erizki13@gmail.com>
2024-09-23 12:52:21 +0800

    revisi Detail Kelas

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ResetPassword from "./pages/auth/ResetPassword";
 import Register from "./pages/auth/Register";
 import SendEmail from "./pages/auth/sendEmail";
 import { HalamnDetailKelas } from "./components/HalamnDetailKelas";
+import { DetailKelas } from "./pages/kelas/DetailKelas"
 // import sendEmail from "./pages/auth/sendEmail";
 
 function App() {
@@ -23,6 +24,8 @@ function App() {
           <Route path="send-email" element={<SendEmail/>} />
           {/* Rute ke halaman detail kelas */}
           <Route path="/detail-kelas" element={<HalamnDetailKelas />} />
+          {/* route detail kelas */}
+          <Route path="/detail_kelas" element={<DetailKelas />} />
 
           {/* NotFound */}
           <Route path="/*" element={<NotFound />} />
