2475f047e63fb63b31622601bab39f5fafeb0e7e
unknown <tengku.rayhan26@gmail.com>
2024-10-02 13:02:40 +0800

    modify isi

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Login from "./pages/auth/Login";
 import ResetPassword from "./pages/auth/ResetPassword";
 import Register from "./pages/auth/Register";
 import SendEmail from "./pages/auth/sendEmail";
-import SuccessPage from "./pages/payment/SuccesPayment";
+import SuccessPage from "./pages/payment/SuccessPage";
 import PaymentPage from "./pages/payment/PaymentPage";
 import DetailKelas from "./pages/Detail/DetailKelas";
 import MainProfile from "./pages/Profile/MainProfile";
@@ -18,6 +18,7 @@ import MulaiKelas from "./pages/MulaiKelas/MulaiKelas";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import AdminDataKelas from "./pages/admin/DataKelas";
 import AdminDataInstuktur from "./pages/admin/AdminDataInstruktur";
+import AdminDataModule from "./pages/admin/AdminDataModule";
 // import NoAccessToken from "./security/NoAccessToken";
 
 function App() {
@@ -88,6 +89,7 @@ function AppRoutes() {
       <Route path="/admin/dashboard" element={<AdminDashboard />} />
       <Route path="/admin/data-kelas" element={<AdminDataKelas />} />
       <Route path="/admin/data-instruktur" element={<AdminDataInstuktur />} />
+      <Route path="/admin/data-module" element={<AdminDataModule />} />
 
       {/* NotFound */}
       <Route path="/*" element={<NotFound />} />
