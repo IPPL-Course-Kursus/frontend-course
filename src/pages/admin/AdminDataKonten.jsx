759d56f90d9af62f23f81c9264565e2078293b3d
nasyaimutdanlucu <nasyaadea@gmail.com>
2024-10-04 01:26:20 +0800

    memperbarui data konten

--- src/pages/admin/AdminDataKonten.jsx
+++ src/pages/admin/AdminDataKonten.jsx
@@ -22,7 +22,7 @@ const AdminDataKonten = () => {
     };
 
     const handleEdit = (content) => {
-        setCurrentContent(content);
+        setCurrentContent(null);
         setIsEditMode(true);
         setShowModal(true);
     };
@@ -46,9 +46,10 @@ const AdminDataKonten = () => {
     };
 
     const confirmDelete = () => {
-        console.log('Hapus Konten:', contentToDelete);
-        setShowDeleteModal(false);
+      setContent(content.filter((c) => c.id !== contentToDelete.id));
+      setShowDeleteModal(false);
     };
+  
 
     const handleDetail = (content) => {
         setCurrentContent(content);
@@ -57,23 +58,24 @@ const AdminDataKonten = () => {
 
     const toggleDetailModal = () => setShowDetailModal(!showDetailModal);
 
+
     return (
-      <div className="flex min-h-screen bg-white">
+      <div className="flex flex-col min-h-screen bg-white">
         <main className="flex-grow p-4 md:p-8">
-          <div className="bg-white rounded-lg shadow-lg p-4 relative min-h-[80vh] flex flex-col">
-            <header className="mb-10">
-            <h2 className="text-2xl md:text-3x1 font-bold text-blue-800 bg-blue-50 p-8 mb-5 rounded">Hi, Admin!</h2>
+          <div className="bg-white p-4 relative min-h-[80vh] flex flex-col">
+            <header className="mb-4">
+              <h2 className="text-2xl md:text-3x1 font-bold text-blue-800 bg-blue-50 p-8 mb-12 rounded">Hi, Admin!</h2>
             
-            <div className="flex flex-col md:flex-row justify-between items-center mb-2">
-            <h3 className="text-xl md:text-2xl font-bold">Data Konten</h3>
-            <button
-              className="w-full md:w-40 font-bold text-white text-sm bg-blue-800 hover:bg-blue-600 md:text-base mx-1 mt-2 px-4 py-2 rounded-full shadow-lg flex items-center justify-center"
-              onClick={handleAdd}
-            >
-              <span className="text-lg">+</span>
-              <span className="ml-2">Tambah</span>
-            </button>
-            </div>
+              <div className="flex flex-col md:flex-row justify-between items-center mb-1">
+                <h3 className="text-xl md:text-2xl font-bold">Data Konten</h3>
+
+                <button className="w-full md:w-40 font-bold text-white text-sm bg-blue-800 hover:bg-blue-600 md:text-base mx-1 mt-2 px-4 py-2 rounded-full shadow-lg flex items-center justify-center"
+                onClick={handleAdd}
+                >
+                <span className="text-lg">+</span>
+                <span className="ml-2">Tambah</span>
+                </button>
+              </div>
             </header>
 
             <table className="content-table w-full border-collapse">
@@ -98,20 +100,19 @@ const AdminDataKonten = () => {
                   <td className="text-left px-4 py-3 md:px-4">
                     
                     <div className="flex flex-col space-y-2 md:space-y-0 md:flex-row md:space-x-2">
-                      <button
-                      className="bg-red-600 hover:bg-red-600 text-black font-semibold py-1 px-3 rounded"
+                      <button className="bg-red-600 hover:bg-red-600 text-black font-semibold py-1 px-3 rounded"
                       onClick={() => handleEdit(c)}
                       >
                       Ubah
                       </button>
-                      <button
-                      className="bg-red-600 hover:bg-red-600 text-black font-semibold py-1 px-3 rounded"
+
+                      <button className="bg-red-600 hover:bg-red-600 text-black font-semibold py-1 px-3 rounded"
                       onClick={() => handleDetail(c)}
                       >
                       Detail
                       </button>
-                      <button
-                      className="bg-red-600 hover:bg-red-600 text-black font-semibold py-1 px-3 rounded"
+
+                      <button className="bg-red-600 hover:bg-red-600 text-black font-semibold py-1 px-3 rounded"
                       onClick={() => handleDelete(c)}
                       >
                       Hapus
@@ -128,7 +129,7 @@ const AdminDataKonten = () => {
         {/* Modal untuk Tambah/Edit */}
         {showModal && (
           <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center">
-            <div className="bg-white w-11/12 md:w-2/3 p-6 rounded-3xl shadow-lg relative">
+            <div className="bg-white w-11/12 md:w-2/3 lg:w-2/3 p-6 rounded-3xl shadow-lg relative overflow-y-auto">
               <button
                 className="absolute top-2 right-2 text-gray-600 hover:text-gray-800"
                 onClick={toggleModal}
@@ -191,7 +192,7 @@ const AdminDataKonten = () => {
         {/* Modal Detail Kelas */}
         {showDetailModal && (
           <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center">
-            <div className="bg-white w-3/4 md:w-1/2 h-auto p-4 rounded-3xl shadow-lg relative">
+            <div className="bg-white w-11/12 md:w-2/3 lg:w-1/2 max-h-[90vh] p-6 rounded-3xl shadow-lg relative overflow-y-auto">
               <button
                 className="absolute top-2 right-2 text-gray-600 hover:text-gray-800"
                 onClick={toggleDetailModal}
@@ -201,7 +202,7 @@ const AdminDataKonten = () => {
               <h2 className="text-center text-xl md:text-2xl font-bold mb-6 text-blue-600">
                 Detail Kelas
               </h2>
-              <form className="max-w-lg mx-auto">
+              <form className="max-w-lg mx-auto grid grid-cols-1 gap-4">
                 {/* Id Konten */}
                 <div className="mb-4">
                   <label className="block text-gray-700">Id Konten</label>
