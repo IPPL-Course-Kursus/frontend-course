4e688be179c9601540df163709089906976d7eca
Muhammad Ricky Zakaria <132816291+MRickyyZ@users.noreply.github.com>
2024-10-17 21:56:15 +0800

    fix 1 create, update data kategori (#65)
    Co-authored-by: Erizki0712 <erizki13@gmail.com>

--- src/components/KategoriComponents/TambahKategori.jsx
+++ src/components/KategoriComponents/TambahKategori.jsx
@@ -63,6 +63,88 @@
 
 // File: ../../components/KategoriComponents/TambahKategori.jsx
 
+// import PropTypes from "prop-types";
+// import { useState } from "react";
+// import { useDispatch } from "react-redux";
+// import KategoriForm from "./KategoriForm";
+// import { addCategory } from "../../redux/actions/adminDataKategoriActions";
+
+// const TambahKategori = ({ show, onClose }) => {
+//   const dispatch = useDispatch();
+//   const [formData, setFormData] = useState({
+//     categoryName: "",
+//     published: false, // or true, depending on your default
+//     // ...other fields if any...
+//   });
+//   const [imagePreview, setImagePreview] = useState(null);
+//   const [imageFile, setImageFile] = useState(null);
+
+//   // Handle text input changes and checkbox
+//   const handleInputChange = (e) => {
+//     const { name, value, type, checked } = e.target;
+
+//     if (type === "checkbox") {
+//       setFormData({ ...formData, [name]: checked });
+//     } else {
+//       setFormData({ ...formData, [name]: value });
+//     }
+//   };
+
+//   // Handle image upload and preview
+//   const handleImageUpload = (e) => {
+//     const file = e.target.files[0];
+//     if (file) {
+//       setImageFile(file);
+
+//       const reader = new FileReader();
+//       reader.onload = () => {
+//         setImagePreview(reader.result);
+//       };
+//       reader.readAsDataURL(file);
+//     }
+//   };
+
+//   const handleAdd = (e) => {
+//     e.preventDefault();
+//     console.log("Adding new category with data:", formData);
+
+//     const formDataToSend = new FormData();
+//     formDataToSend.append("categoryName", formData.categoryName);
+//     formDataToSend.append("published", formData.published);
+
+//     if (imageFile) {
+//       formDataToSend.append("image", imageFile);
+//     }
+
+//     // Append other fields if any
+
+//     dispatch(addCategory(formDataToSend));
+//     onClose();
+//   };
+
+//   return (
+//     <KategoriForm
+//       show={show}
+//       onClose={onClose}
+//       formData={formData}
+//       handleInputChange={handleInputChange}
+//       handleImageUpload={handleImageUpload}
+//       handleSubmit={handleAdd}
+//       imagePreview={imagePreview}
+//       isEditMode={false}
+//     />
+//   );
+// };
+
+// TambahKategori.propTypes = {
+//   show: PropTypes.bool.isRequired,
+//   onClose: PropTypes.func.isRequired,
+// };
+
+// export default TambahKategori;
+
+// TambahKategori.jsx
+
 import PropTypes from "prop-types";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
@@ -73,8 +155,8 @@ const TambahKategori = ({ show, onClose }) => {
   const dispatch = useDispatch();
   const [formData, setFormData] = useState({
     categoryName: "",
+    categoryCode: "",
     published: false, // or true, depending on your default
-    // ...other fields if any...
   });
   const [imagePreview, setImagePreview] = useState(null);
   const [imageFile, setImageFile] = useState(null);
@@ -108,15 +190,15 @@ const TambahKategori = ({ show, onClose }) => {
     e.preventDefault();
     console.log("Adding new category with data:", formData);
 
+    // Construct FormData
     const formDataToSend = new FormData();
     formDataToSend.append("categoryName", formData.categoryName);
+    formDataToSend.append("categoryCode", formData.categoryCode);
     formDataToSend.append("published", formData.published);
 
     if (imageFile) {
       formDataToSend.append("image", imageFile);
     }
-
-    // Append other fields if any
 
     dispatch(addCategory(formDataToSend));
     onClose();
