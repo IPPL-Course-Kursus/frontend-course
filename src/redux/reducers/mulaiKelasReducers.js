467ef6c6a03e24b6143b20093c9c9789d743d021
Muhammad Ricky Zakaria <132816291+MRickyyZ@users.noreply.github.com>
2024-10-28 17:50:40 +0800

    Rickyy (#95)
    * commit
    
    * .
    
    * ..
    
    * fix component navbar
    
    * fetching transaction dengan midtrans
    
    * fix navigate navbar
    
    * add sweet alert for free course
    
    * fix conflict
    
    * commit
    
    * fetch topik-kelas and add search bar
    
    * memperbaiki navigate  page NotFound
    
    * fetching card rekomendasi
    
    * fix navigate card topik-kelas
    
    * commit
    
    * fix navbar
    
    * fix navbar
    
    * fix navbarrr
    
    * commit
    
    * add function filter
    
    * get level n type
    
    * commit
    
    * fix whiteScreen dataIntruktur & mulaiKelas
    
    * fix navigate lihatSemua-home & cardReccomended
    
    * fetch mulai kelas
    
    * fetch progress bar
    
    * fix materi detail-kelas
    
    * add fitur interpreter mulai kelas

--- src/redux/reducers/mulaiKelasReducers.js
+++ src/redux/reducers/mulaiKelasReducers.js
@@ -1,34 +1,42 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  data: null,
-  loading: false,
-  error: null,
+    data: null,
+    output:'',
+    loading: false,
+    error: null,
 };
 
 const mulaiKelasSlice = createSlice({
-  name: "mulaikelas",
-  initialState,
-  reducers: {
-    mulaiKelasRequest: (state) => {
-      state.loading = true;
-      state.error = null;
+    name: "mulaikelas",
+    initialState,
+    reducers: {
+        mulaiKelasRequest: (state) => {
+            state.loading = true;
+            state.error = null;
+        },
+        mulaiKelasSuccess: (state, action) => {
+            state.loading = false;
+            state.data = action.payload;
+            state.courseStatus = action.payload.courseStatus;
+            state.contentFinish = action.payload.contentFinish;
+            state.totalContent = action.payload.totalContent;
+        },
+        mulaiKelasFailure: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
+        runCodeSuccess: (state, action) => {
+            state.loading = false;
+            state.output = action.payload.output;  
+        },
+        resetOutput: (state) => {
+            state.output = null;
+        },
     },
-    mulaiKelasSuccess: (state, action) => {
-      state.loading = false;
-      state.data = action.payload;
-    },
-    mulaiKelasFailure: (state, action) => {
-      state.loading = false;
-      state.error = action.payload;
-    },
-  },
 });
 
-export const {
-  mulaiKelasRequest,
-  mulaiKelasSuccess,
-  mulaiKelasFailure,
-} = mulaiKelasSlice.actions;
+export const { mulaiKelasRequest, mulaiKelasSuccess, mulaiKelasFailure, runCodeSuccess, resetOutput } =
+    mulaiKelasSlice.actions;
 
 export default mulaiKelasSlice.reducer;
