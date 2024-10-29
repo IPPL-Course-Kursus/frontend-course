52b82e7b713ea510ef63e4fb2886c0c62efb6856
gunturrr <gunturwisnu72@gmail.com>
2024-10-30 00:00:13 +0800

    fix home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -59,7 +59,11 @@ const Home = () => {
       <div className="w-full h-auto bg-primary flex justify-center items-center py-12 mt-20">
         <div className="flex flex-col lg:flex-row items-center max-w-6xl px-6 lg:px-12">
           <div className="lg:w-1/2">
-            <img src={imgSection} alt="Freelancer Section" className="w-full h-auto" />
+            <img
+              src="https://images.unsplash.com/photo-1506878206813-92402b8ded23?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Freelancer Section"
+              className="w-full h-auto"
+            />
           </div>
           <div className="lg:w-1/2 text-center lg:text-left mt-6 lg:mt-0 lg:pl-12">
             <h2 className="text-white text-3xl font-semibold mb-4">
