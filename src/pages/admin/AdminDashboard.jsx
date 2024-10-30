6f7955654359e0df54ba18fbf211d6cc681b2bb5
aqiilaah <aqiilaah25@gmail.com>
2024-10-30 13:25:16 +0800

    modify

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { FaUsers, FaSearch, FaFilter, FaBars } from "react-icons/fa";
+import { IoArrowBackCircle, IoArrowForwardCircle } from "react-icons/io5";
 import SideBar from "../../components/Sidebar/SidebarAdmin";
 import { fetchStats, fetchPayments, fetchuser } from "../../redux/actions/adminDashboardActions";
 
@@ -13,32 +14,28 @@ const AdminDashboard = () => {
   const [filter, setFilter] = useState("");
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [searchVisible, setSearchVisible] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1); // State untuk halaman saat ini
-  const itemsPerPage = 5; // Jumlah item per halaman
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
 
-  // Fetch stats and payment data when the component mounts
   useEffect(() => {
     dispatch(fetchStats());
     dispatch(fetchPayments());
     dispatch(fetchuser());
-    console.log(paymentStatus);
   }, [dispatch]);
 
-  // Calculate free and premium class counts
   const freeClassesCount = paymentStatus.filter(payment => payment.paymentMethod === "Free").length;
   const premiumClassesCount = paymentStatus.filter(payment => payment.paymentMethod !== "Free").length;
 
-  // Filter payments based on search inputs and filter
   const filteredPayments = paymentStatus.filter((payment) => {
     const isGlobalSearchMatch =
       globalSearch === "" ||
-      payment.id && payment.id.toString().includes(globalSearch.toLowerCase()) ||  // Convert id to string
+      payment.id && payment.id.toString().includes(globalSearch.toLowerCase()) ||  
       payment.courseName && payment.courseName.toLowerCase().includes(globalSearch.toLowerCase()) ||
-      payment.paymentMethod && payment.paymentMethod. toLowerCase().includes(globalSearch.toLowerCase());
+      payment.paymentMethod && payment.paymentMethod.toLowerCase().includes(globalSearch.toLowerCase());
 
     const isPaymentSearchMatch =
       paymentSearch === "" ||
-      payment.id && payment.id.toString().includes(paymentSearch.toLowerCase()) ||  // Convert id to string
+      payment.id && payment.id.toString().includes(paymentSearch.toLowerCase()) ||  
       payment.courseName && payment.courseName.toLowerCase().includes(paymentSearch.toLowerCase()) ||
       payment.paymentMethod && payment.paymentMethod.toLowerCase().includes(paymentSearch.toLowerCase());
 
@@ -47,10 +44,8 @@ const AdminDashboard = () => {
     return isGlobalSearchMatch && isPaymentSearchMatch && isFilterMatch;
   });
 
-  // Sort payments by ID in ascending order (from 1 upwards)
   const sortedPayments = filteredPayments.sort((a, b) => a.id - b.id);
 
-  // Pagination logic
   const totalPages = Math.ceil(sortedPayments.length / itemsPerPage);
   const paginatedPayments = sortedPayments.slice(
     (currentPage - 1) * itemsPerPage,
@@ -70,20 +65,18 @@ const AdminDashboard = () => {
   };
 
   const handleFilterChange = (e) => setFilter(e.target.value);
-
   const toggleSearch = () => setSearchVisible((prev) => !prev);
 
   return (
     <div className="flex min-h-screen">
-      {/* Sidebar */}
       <div
         className={`fixed inset-0 z-50 transition-transform transform bg-white md:relative md:translate-x-0 md:bg-transparent h-screen flex flex-col ${
-      sidebarOpen ? "translate-x-0" : "-translate-x-full"}`}
+          sidebarOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
       >
         <SideBar />
       </div>
 
-      {/* Overlay */}
       {sidebarOpen && (
         <div
           className="fixed inset-0 z-40 md:hidden"
@@ -91,11 +84,8 @@ const AdminDashboard = () => {
         />
       )}
 
-      {/* Main content */}
       <div className="flex-1 flex flex-col p-4 md:p-6 bg-secondary min-h-screen">
-        {/* Header */}
         <div className="fixed top-0 right-0 w-full md:w-auto md:left-[255px] bg-[#F3F7FB] p-6 flex justify-between items-center shadow-sm z-30">
-          {/* Menu button - visible on mobile */}
           <button
             className="text-[#0a61aa] md:hidden"
             onClick={() => setSidebarOpen((prev) => !prev)}
@@ -105,7 +95,6 @@ const AdminDashboard = () => {
 
           <h1 className="text-2xl font-bold text-[#173D94]">Hi, Admin!</h1>
 
-          {/* Global Search Bar */}
           <div className="relative w-right max-w-xs md:max-w-sm lg:max-w-md hidden md:block">
             <input
               type="text"
@@ -120,7 +109,6 @@ const AdminDashboard = () => {
           </div>
         </div>
 
-        {/* Cards Users */}
         <div className="mt-[80px] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
           {[{ count: user.userCount, label: "Users", color: "bg-primary" },
           { count: user.instrukturCount, label: "Instruktor", color: "bg-success" },
@@ -139,14 +127,10 @@ const AdminDashboard = () => {
             ))}
         </div>
 
-        {/* Section for Payment Status */}
         <div className="flex flex-col md:flex-row justify-between items-center mb-4 space-y-4 md:space-y-0">
-          {/* Title */}
           <h2 className="mt-4 text-lg md:text-xl font-bold text-neutral05">Status Pembayaran</h2>
 
-          {/* Filter and Search */}
           <div className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-2">
-            {/* Filter Dropdown */}
             <div className="relative">
               <select
                 value={filter}
@@ -160,7 +144,6 @@ const AdminDashboard = () => {
               <FaFilter className="absolute right-4 top-2 text-[#173D94] text-sm" />
             </div>
 
-            {/* Search Icon */}
             <div className="relative w-full md:w-auto flex items-center">
               <FaSearch
                 className="text-[#173D94] text-lg cursor-pointer"
@@ -178,7 +161,6 @@ const AdminDashboard = () => {
           </div>
         </div>
 
-        {/* Payment Status Table */}
         <div className="overflow-x-auto bg-white p-4 mt-2">
           <table className="min-w-full table-auto">
             <thead>
@@ -192,41 +174,42 @@ const AdminDashboard = () => {
               </tr>
             </thead>
             <tbody>
-                {paginatedPayments.map((payment) => (
-                  <tr key={payment.id} className="border-b">
-                    <td className="px-2 md:px-4 py-2">{payment.id}</td>
-                    <td className="px-2 md:px-4 py-2">{payment.courseName}</td>
-                    <td className="px-2 md:px-4 py-2">Rp. {payment.totalPrice}</td>
-                    <td className="px-2 md:px-4 py-2">{payment.paymentStatus}</td>
-                    <td className="px-2 md:px-4 py-2">{payment.paymentMethod}</td>
-                    <td className="px-2 md:px-4 py-2">{payment.updatedAt}</td>
-                  </tr>
-                ))}
+              {paginatedPayments.map((payment) => (
+                <tr key={payment.id} className="border-b">
+                  <td className="px-2 md:px-4 py-2">{payment.id}</td>
+                  <td className="px-2 md:px-4 py-2">{payment.courseName}</td>
+                  <td className="px-2 md:px-4 py-2">Rp. {payment.totalPrice}</td>
+                  <td className="px-2 md:px-4 py-2">{payment.paymentStatus}</td>
+                  <td className="px-2 md:px-4 py-2">{payment.paymentMethod}</td>
+                  <td className="px-2 md:px-4 py-2">{payment.updatedAt}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
 
-        {/* Pagination Controls */}
-        <div className="flex justify-between items-center mt-2">
+          {/* Pagination Controls */}
+          <div className="flex justify-between items-center mt-4">
             <button
-              className={`text-sm px-1 py-1 bg-primary-300 text-secondary-600 ${currentPage === 1 && "opacity-50 pointer-events-none"
-                }`}
+              className={`flex items-center py-2 px-4 rounded-lg ${currentPage === 1 ? "bg-gray-300 cursor-not-allowed" : "bg-[#0a61aa] text-white"} transition-all duration-300 hover:scale-105`}
               onClick={goToPreviousPage}
+              disabled={currentPage === 1}
             >
+              <IoArrowBackCircle className="mr-2 text-xl" />
               Previous
             </button>
-            <div>
+
+            <span className="text-lg font-semibold">
               Page {currentPage} of {totalPages}
-            </div>
+            </span>
+
             <button
-              className={`text-sm px-1 py-1 bg-primary-300 text-secondary-600 ${currentPage === totalPages && "opacity-50 pointer-events-none"
-                }`}
+              className={`flex items-center py-2 px-4 rounded-lg ${currentPage === totalPages ? "bg-gray-300 cursor-not-allowed" : "bg-[#0a61aa] text-white"} transition-all duration-300 hover:scale-105`}
               onClick={goToNextPage}
+              disabled={currentPage === totalPages}
             >
               Next
+              <IoArrowForwardCircle className="ml-2 text-xl" />
             </button>
-            {/* <tr>
-                  <td colSpan="6" className="text-center py-4">Loading...</td>
-            </tr> */}
           </div>
         </div>
       </div>
