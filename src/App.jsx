1a069485dd5ffe26de36763c21272357f633e74c
MRickyyZ <rickykudou@gmail.com>
2024-12-13 02:10:31 +0800

    protect mulai-kelas

--- src/App.jsx
+++ src/App.jsx
@@ -135,12 +135,11 @@ function AppRoutes() {
       <Route
         path="/mulai-kelas/:id"
         element={
-          // <NoAccesToken>
+          <NoAccesToken>
             <MulaiKelas />
-          // </NoAccesToken>
+          </NoAccesToken>
         }
       />
-
       <Route path="/course-detail/:id" element={<DetailKelas />} />
       <Route path="/topik-kelas" element={<TopikKelas />} />
 
