2ef39775d57493c8b47923fb5381a3d26d8571ca
febrynn <11221008@student.itk.ac.id>
2024-12-23 08:51:38 +0800

    menghapus error diatas tabel menggunakan tostify, ubah popup

--- src/pages/admin/KelolaDataAdmin/AdminDataType.jsx
+++ src/pages/admin/KelolaDataAdmin/AdminDataType.jsx
@@ -1,110 +1,133 @@
 import { useState, useEffect } from "react";
 import Sidebar from "../../../components/Sidebar/SidebarAdmin";
-import { FaBars } from "react-icons/fa";
-import { IoAddCircleOutline } from "react-icons/io5";
-import NavbarAdmin from "../../../components/NavbarAdmin";
-
-// Import Redux hooks and actions
-import { useDispatch, useSelector } from "react-redux";
+import { FaSearch } from "react-icons/fa";
 import {
-  getAllTypeCourses,
-  createTypeCourse,
-  updateTypeCourseById,
-  deleteTypeCourseById,
-} from "../../../redux/actions/typeCourseActions";
+  IoAddCircleOutline,
+  IoArrowBackCircle,
+  IoArrowForwardCircle,
+} from "react-icons/io5";
+import NavbarAdmin from "../../../components/NavbarAdmin";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllTypeCourses, createTypeCourse, updateTypeCourseById, deleteTypeCourseById } from "../../../redux/actions/typeCourseActions";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// Import komponen baru
+import DataTypeInput from "../../../components/Admin/DataType/DataTypeInput";
+import DataTypeUbah from "../../../components/Admin/DataType/DataTypeUbah";
+import DeleteType from "../../../components/Admin/DataType/deletetype"; // Import DeleteType
 
 const AdminDataType = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
-
-  // State for modal visibility and form data
   const [showModal, setShowModal] = useState(false);
   const [isEditMode, setIsEditMode] = useState(false);
   const [selectedType, setSelectedType] = useState(null);
-  const [formData, setFormData] = useState({
-    typeName: "",
-  });
+  const [formData, setFormData] = useState({ typeName: "" });
+  const [showDeleteModal, setShowDeleteModal] = useState(false); // State to control the delete modal visibility
 
-  // State for popup notification
-  const [showNotification, setShowNotification] = useState(false);
-  const [notificationMessage, setNotificationMessage] = useState("");
-
-  // Redux dispatch and selector
   const dispatch = useDispatch();
-  const { typeCourses, loading, error } = useSelector(
-    (state) => state.typeCourse
-  );
+  const { typeCourses, loading, error } = useSelector((state) => state.typeCourse);
 
-  // Fetch type courses on component mount
   useEffect(() => {
     dispatch(getAllTypeCourses());
   }, [dispatch]);
 
-  // Show notification popup
-  const showPopupNotification = (message) => {
-    setNotificationMessage(message);
-    setShowNotification(true);
-    setTimeout(() => {
-      setShowNotification(false);
-    }, 3000);
+ // Fungsi untuk notifikasi dengan Toastify
+ const showNotification = (message, type = "success") => {
+  const options = {
+    style: {
+      borderRadius: "8px",
+      background: type === "success" ? "#4BB543" : "#FF3333",
+      color: "#fff",
+    },
   };
-
+  if (type === "success") toast.success(message, options);
+  else toast.error(message, options);
+};
   // Handle form input change
   const handleInputChange = (e) => {
     setFormData({ ...formData, typeName: e.target.value });
   };
 
-  // Handle add type
   const handleAddType = async () => {
+    if (!isNaN(formData.typeName)) {
+      showNotification("tidak boleh  berupa angka", "error");
+      return;
+    }
+
+    const regex = /^(?=.*[a-zA-Z])(?=.*\d)/;
+
+  if (regex.test(formData.typeName)) {
+    showNotification("Nama tipe tidak boleh mengandung kombinasi huruf dan angka", "error");
+    return;
+  }
+  
+    // Cek apakah input memiliki minimal 3 huruf
+    if (formData.typeName.length < 3) {
+      showNotification("Nama tipe harus terdiri dari minimal 3 huruf", "error");
+      return;
+    }
+  
     if (
       typeCourses.some(
         (type) =>
           type.typeName.toLowerCase() === formData.typeName.toLowerCase()
       )
     ) {
-      showPopupNotification("Type sudah ada");
+      showNotification("Tipe sudah ada", "error");
       return;
     }
+  
     try {
       await dispatch(createTypeCourse(formData.typeName));
       setFormData({ typeName: "" });
       setShowModal(false);
-      showPopupNotification("Tipe berhasil ditambahkan");
-      dispatch(getAllTypeCourses()); // Refresh the list after addition
+      showNotification("Tipe berhasil ditambahkan");
+      dispatch(getAllTypeCourses());
     } catch (err) {
-      showPopupNotification("Terjadi kesalahan saat menambahkan tipe");
+      showNotification("Terjadi kesalahan saat menambahkan tipe", "error");
     }
   };
-
-  // Handle edit type
+  
   const handleEditType = async () => {
+    if (!isNaN(formData.typeName)) {
+      showNotification("Nama tipe tidak boleh hanya berupa angka", "error");
+      return;
+    }
+  
     try {
-      await dispatch(
-        updateTypeCourseById(selectedType.id, formData.typeName)
-      );
+      await dispatch(updateTypeCourseById(selectedType.id, formData.typeName));
       setFormData({ typeName: "" });
       setSelectedType(null);
       setIsEditMode(false);
       setShowModal(false);
-      showPopupNotification("Tipe berhasil diubah");
-      dispatch(getAllTypeCourses()); // Refresh the list after update
+      showNotification("Tipe berhasil diubah");
+      dispatch(getAllTypeCourses());
     } catch (err) {
-      showPopupNotification("Terjadi kesalahan saat mengubah tipe");
+      showNotification("Terjadi kesalahan saat mengubah tipe", "error");
+    }
+  };
+  
+
+  // Handle delete type
+  const handleDeleteType = (id) => {
+    setSelectedType(id); // Set the selected type to be deleted
+    setShowDeleteModal(true); // Show the delete confirmation modal
+  };
+
+  const confirmDeleteType = async () => {
+    try {
+      await dispatch(deleteTypeCourseById(selectedType));
+      showNotification("Tipe berhasil dihapus");
+      dispatch(getAllTypeCourses()); // Refresh the list after deletion
+      setShowDeleteModal(false); // Close the delete modal
+    } catch (err) {
+      showNotification("Terjadi kesalahan saat menghapus tipe", "error");
     }
   };
 
-  // Handle delete type
-  const handleDeleteType = async (id) => {
-    if (
-      window.confirm("Are you sure you want to delete this type course?")
-    ) {
-      try {
-        await dispatch(deleteTypeCourseById(id));
-        showPopupNotification("Tipe berhasil dihapus");
-        dispatch(getAllTypeCourses()); // Refresh the list after deletion
-      } catch (err) {
-        showPopupNotification("Terjadi kesalahan saat menghapus tipe");
-      }
-    }
+  const cancelDeleteType = () => {
+    setShowDeleteModal(false); // Close the delete modal without deleting
   };
 
   // Open modal for adding new type
@@ -126,7 +149,6 @@ const AdminDataType = () => {
   return (
     <>
       <div className="flex">
-        {/* Sidebar */}
         <div
           className={`fixed inset-0 z-50 transition-transform transform bg-white md:relative md:translate-x-0 md:bg-transparent ${
             sidebarOpen ? "translate-x-0" : "-translate-x-full"
@@ -135,7 +157,6 @@ const AdminDataType = () => {
           <Sidebar />
         </div>
 
-        {/* Overlay */}
         {sidebarOpen && (
           <div
             className="fixed inset-0 bg-black opacity-50 z-40 md:hidden"
@@ -146,14 +167,12 @@ const AdminDataType = () => {
         <div className="flex-1 p-4 md:p-6 bg-secondary min-h-screen font-poppins">
           <NavbarAdmin setSidebarOpen={setSidebarOpen} />
 
-          {/* Section Data Type */}
           <div className="flex flex-col md:flex-row justify-between items-center mb-4 space-y-4 md:space-y-0">
             <h2 className="flex items-center py-2 px-4 mt-4 bg-gradient-to-r from-[#FF5722] to-[#FF9800] text-white font-semibold rounded-md text-xl transition-all duration-300 transform hover:scale-105 shadow-lg hover:shadow-xl mb-4">
               Data Type
             </h2>
 
             <div className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-2">
-              {/* Tambah Tipe Button */}
               <div className="relative">
                 <button
                   className="py-1 px-4 bg-[#0a61aa] text-white font-semibold rounded-md text-xs transition-all duration-300 hover:scale-105 flex items-center justify-center"
@@ -166,11 +185,8 @@ const AdminDataType = () => {
             </div>
           </div>
 
-          {/* Tabel Data Type */}
           <div className="overflow-x-auto bg-white p-4">
-            {loading ? (
-              <p>Loading...</p>
-            ) : error ? (
+            {error ? (
               <p className="text-red-500">{error}</p>
             ) : (
               <table className="min-w-full table-auto">
@@ -185,23 +201,16 @@ const AdminDataType = () => {
                   {typeCourses.map((type, index) => {
                     const rowNumber = index + 1;
                     return (
-                      <tr
-                        key={type.id}
-                        className="border-t text-xs md:text-sm"
-                      >
+                      <tr key={type.id} className="border-t text-xs md:text-sm">
                         <td className="px-2 md:px-4 py-2">{rowNumber}</td>
-                        <td className="px-2 md:px-4 py-2">
-                          {type.typeName}
-                        </td>
+                        <td className="px-2 md:px-4 py-2">{type.typeName}</td>
                         <td className="px-2 md:px-4 py-2 flex flex-wrap space-x-2">
-                          {/* Tombol Ubah */}
                           <button
                             className="py-1 px-2 md:px-4 bg-red-500 text-white font-semibold rounded-md text-xs transition-all duration-300 hover:scale-105 mb-2"
                             onClick={() => openEditModal(type)}
                           >
                             Ubah
                           </button>
-                          {/* Tombol Hapus */}
                           <button
                             className="py-1 px-2 md:px-4 bg-red-500 text-white font-semibold rounded-md text-xs transition-all duration-300 hover:scale-105 mb-2"
                             onClick={() => handleDeleteType(type.id)}
@@ -217,7 +226,6 @@ const AdminDataType = () => {
             )}
           </div>
 
-          {/* Modal for Adding and Editing Type */}
           {showModal && (
             <div
               className="fixed inset-0 flex justify-center items-center z-50"
@@ -234,51 +242,72 @@ const AdminDataType = () => {
                   {isEditMode ? "Ubah Tipe Kelas" : "Tambah Tipe Kelas"}
                 </h2>
 
-                <form
-                  onSubmit={(e) => {
-                    e.preventDefault();
-                    isEditMode ? handleEditType() : handleAddType();
-                  }}
-                >
-                  {/* Type Name Input */}
-                  <div className="mb-4">
-                    <label className="block mb-1 font-semibold">
-                      Tipe Kelas
-                    </label>
-                    <input
-                      type="text"
-                      name="typeName"
-                      value={formData.typeName}
-                      onChange={handleInputChange}
-                      className="w-full p-2 border rounded-xl"
-                      placeholder="Masukkan Nama Tipe"
-                      required
-                    />
-                  </div>
+                {loading ? (
+                  <div className="text-center">Loading...</div> // Tempatkan loading di dalam modal
+                ) : (
+                  <form
+                    onSubmit={(e) => {
+                      e.preventDefault();
+                      isEditMode ? handleEditType() : handleAddType();
+                    }}
+                  >
+                    <div className="mb-4">
+                      <label className="block mb-1 font-semibold">
+                        Tipe Kelas
+                      </label>
+                      <input
+                        type="text"
+                        name="typeName"
+                        value={formData.typeName}
+                        onChange={handleInputChange}
+                        className="w-full p-2 border rounded-xl"
+                        placeholder="Masukkan Nama Tipe"
+                        required
+                      />
+                    </div>
 
-                  <div className="flex justify-center">
-                    <button
-                      className="py-2 px-6 bg-[#0a61aa] text-white rounded-xl"
-                      type="submit"
-                    >
-                      {isEditMode ? "Ubah" : "Tambah"}
-                    </button>
-                  </div>
-                </form>
+                    <div className="flex justify-center">
+                      <button
+                        className={`py-2 px-6 rounded-xl ${
+                          formData.typeName === selectedType?.typeName ||
+                          formData.typeName.trim() === ""
+                            ? "bg-gray-400"
+                            : "bg-[#0a61aa]"
+                        } text-white`}
+                        type="submit"
+                        disabled={formData.typeName === selectedType?.typeName || formData.typeName.trim() === ""}
+                      >
+                        {isEditMode ? "Ubah" : "Tambah"}
+                      </button>
+                    </div>
+                  </form>
+                )}
               </div>
             </div>
           )}
 
-          {/* Notification Popup */}
-          {showNotification && (
-            <div
-              className={`fixed bottom-0 left-1/2 transform -translate-x-1/2 mb-10 bg-red-500 text-white px-4 py-2 rounded-md shadow-lg transition-transform duration-500 ease-in-out ${
-                showNotification ? "translate-y-0" : "translate-y-full"
-              }`}
-            >
-              {notificationMessage}
-            </div>
+          {/* Delete Confirmation Modal */}
+          {showDeleteModal && (
+            <DeleteType
+              show={showDeleteModal}
+              onClose={cancelDeleteType}
+              onConfirm={confirmDeleteType}
+            />
           )}
+
+          <ToastContainer 
+           position="bottom-center"
+           autoClose={3000}
+           hideProgressBar
+           newestOnTop
+           closeOnClick
+           rtl={false}
+           closeButton= {false}
+           pauseOnFocusLoss
+           draggable
+           pauseOnHover
+           
+           theme="colored"/>
         </div>
       </div>
     </>
