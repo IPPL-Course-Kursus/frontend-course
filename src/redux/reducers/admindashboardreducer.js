dd680cb6d93590f423c1625ea2feeed7292d3100
aqiilaah <aqiilaah25@gmail.com>
2024-12-29 01:14:52 +0800

    fix fetch, tambahkan cards total transaksi dan metode pembayaran + fetch

--- src/redux/reducers/admindashboardreducer.js
+++ src/redux/reducers/admindashboardreducer.js
@@ -1,70 +1,72 @@
-// admindashboardreducer.js
 const initialState = {
-    stats: {},
-    paymentStatus: [],
-    user: {},
-    loading: false,
-    error: null,
+  stats: {},
+  paymentStatus: [],
+  user: {},
+  loading: false,
+  error: null,
 };
 
 const adminDashboardReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "FETCH_STATS_REQUEST":
-      return {
-        ...state,
-        loading: true,
-      };
-    case "FETCH_STATS_SUCCESS":
+switch (action.type) {
+  case "FETCH_STATS_REQUEST":
+    return {
+      ...state,
+      loading: true,
+    };
+  case "FETCH_STATS_SUCCESS":
+    return {
+      ...state,
+      loading: false,
+      stats: action.payload,
+    };
+  case "FETCH_STATS_FAILURE":
+    return {
+      ...state,
+      loading: false,
+      error: action.payload,
+    };
+  case "FETCH_PAYMENTS_REQUEST":
+    return {
+      ...state,
+      loading: true,
+    };
+    case "FETCH_PAYMENTS_SUCCESS":
       return {
         ...state,
         loading: false,
-        stats: action.payload,
+        paymentStatus: action.payload, // Langsung menyimpan transaksi ke paymentStatus
       };
-    case "FETCH_STATS_FAILURE":
-      return {
-        ...state,
-        loading: false,
-        error: action.payload,
-      };
-    case "FETCH_PAYMENTS_REQUEST":
-      return {
-        ...state,
-        loading: true,
-      };
-      case "FETCH_PAYMENTS_SUCCESS":
-        return {
-          ...state,
-          loading: false,
-          paymentStatus: action.payload.data, // Ambil data dari payload
-        };
+    
+    
       
-    case "FETCH_PAYMENTS_FAILURE":
+    
+  case "FETCH_PAYMENTS_FAILURE":
+    return {
+      ...state,
+      loading: false,
+      error: action.payload,
+    };
+
+    case "FETCH_user_REQUEST":
+      return {
+        ...state,
+        loading: true,
+      };
+    case "FETCH_user_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        user: action.payload.data,
+      };
+    case "FETCH_user_FAILURE":
       return {
         ...state,
         loading: false,
         error: action.payload,
       };
-
-      case "FETCH_user_REQUEST":
-        return {
-          ...state,
-          loading: true,
-        };
-      case "FETCH_user_SUCCESS":
-        return {
-          ...state,
-          loading: false,
-          user: action.payload.data,
-        };
-      case "FETCH_user_FAILURE":
-        return {
-          ...state,
-          loading: false,
-          error: action.payload,
-        };
-            default:
-                return state;
-  }
+          default:
+              return state;
+}
 };
 
 export default adminDashboardReducer;
