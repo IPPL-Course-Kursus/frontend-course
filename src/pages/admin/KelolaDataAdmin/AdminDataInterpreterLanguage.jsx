25c8e55c8f309e9c99c4bf27d3a380f589759d77
Erizki0712 <erizki13@gmail.com>
2024-12-29 18:03:57 +0800

    color

--- src/pages/admin/KelolaDataAdmin/AdminDataInterpreterLanguage.jsx
+++ src/pages/admin/KelolaDataAdmin/AdminDataInterpreterLanguage.jsx
@@ -178,7 +178,7 @@ const AdminDataInterpreterLanguage = () => {
                           <td className="px-2 md:px-4 py-2 flex space-x-2">
                             {/* Tombol Ubah */}
                             <button
-                              className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-md text-sm transition-all duration-300 hover:bg-blue-600"
+                              className="py-2 px-4 bg-green-500 text-white font-semibold rounded-md text-sm transition-all duration-300 hover:bg-blue-600"
                               onClick={() => openEditModal(language)}
                             >
                               Ubah
--- src/pages/admin/KelolaDataAdmin/AdminDataKategori.jsx
+++ src/pages/admin/KelolaDataAdmin/AdminDataKategori.jsx
@@ -254,7 +254,7 @@ const AdminDataKategori = () => {
                         <td className="px-2 md:px-4 py-2 flex space-x-2">
                           {/* Edit Button */}
                           <button
-                            className="py-2 px-3 md:px-4 bg-blue-500 text-white font-semibold rounded-md text-sm transition-all duration-300 hover:bg-blue-600"
+                            className="py-2 px-3 md:px-4 bg-green-500 text-white font-semibold rounded-md text-sm transition-all duration-300 hover:bg-blue-600"
                             onClick={() => handleEditClick(category)}
                           >
                             Ubah
